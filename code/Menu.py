9f42ec80f42e7d4881f066d2d07c8b9e1a2a03d2
caeti <caetiteleticia@gmail.com>
2026-03-22 11:51:39 -0300

    fixing import in level.py file

--- code/Menu.py
+++ code/Menu.py
@@ -11,7 +11,7 @@ from code.Const import WIN_WIDTH, COLOR_BLACK, MENU_OPTION, COLOR_BLUE, COLOR_RE
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/Menu1.png')
+        self.surf = pygame.image.load('./asset/Menu1.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
